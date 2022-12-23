0cf6199a9f8cb3bc2f0ee30d13d53e1c13ee1f8d
1607562910 <1607562910@qq.com>
2022-12-23 09:18:46 +0800

    x

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">
+        <el-button size="mini">Home</el-button>
+      </router-link>
+      |
+      <router-link to="/about">
+        <el-button size="mini">About</el-button>
+      </router-link>
     </div>
     <router-view/>
   </div>
