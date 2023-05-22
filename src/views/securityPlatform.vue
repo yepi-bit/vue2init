bf2bf211b11b7f64932cfcb63948ebfca15c3d29
1607562910 <1607562910@qq.com>
2023-05-22 16:29:59 +0800

    x

--- src/views/securityPlatform.vue
+++ src/views/securityPlatform.vue
@@ -22,7 +22,7 @@
     </div>
     <div>
       <el-row>
-        <el-col>
+        <el-col :span="7">
           <div
               style="width: 150px;height: 150px;border-radius: 50%;background: cornflowerblue;">
             <div style="line-height: 150px;text-align: center">
@@ -30,6 +30,9 @@
               <span>{{ nowDate }}</span>
             </div>
           </div>
+        </el-col>
+        <el-col :span="7">
+          <div id="barCj" ref="barCj"></div>
         </el-col>
       </el-row>
     </div>
@@ -45,6 +48,7 @@ export default {
       funnel: null,
       raDar: null,
       bar: null,
+      barCj: null,
       nowDate: '',
       // 数组对象
       milepost: [
@@ -55,6 +59,11 @@ export default {
         {title: '测试', description: '2019.9.1'},
         {title: '发布', description: '2019.11.1'},
         {title: '结项', description: '2019.12.31'},
+      ],
+      CjData: [
+        {value: 110, name: "语文成绩"},
+        {value: 5, name: "数学"},
+        {value: 140.5, name: "英语成绩"},
       ],
       // 默认步骤数
       milepostActive: 5,
@@ -223,6 +232,100 @@ export default {
           }
         ]
       }
+    },
+    barCjOption() {
+      return {
+        title: {
+          zlevel: 2, // 控制圆环图中间的字的层级
+          text: "我是小明的成绩单",
+          top: "47%", // 控制位置
+          left: "32%", // 控制位置
+          textAlign: "center", // 让文字居中
+        },
+        tooltip: {
+          trigger: "item", // 触发机制，鼠标悬浮圆环项上
+          show: true, // 控制鼠标悬浮是否显示数据
+        },
+        legend: {
+          orient: "vertical", // 控制水平排列
+          top: "36%", // 调整位置
+          right: "6%", // 距离右侧位置
+          icon: "circle", // 修改小图标为圆形
+          itemHeight: 12, // 修改圆形小图标的大小
+          textStyle: {
+            // 文字的样式
+            fontSize: 24, // 可控制每个legend项的间距
+            color: "#828282",
+            rich: {
+              // 通过富文本rich给每个项设置样式，下面的oneone、twotwo、threethree可以理解为"每一列"的样式
+              oneone: {
+                // 设置文字、数学、英语这一列的样式
+                width: 50,
+                color: "#000000",
+                fontSize: 12,
+                fontWeight: "bolder",
+              },
+              twotwo: {
+                // 设置10分、20分、30分这一列的样式
+                width: 35,
+                color: "#333",
+                fontSize: 12,
+              },
+              threethree: {
+                // 设置百分比这一列的样式
+                width: 20,
+                color: "#E0E0E0",
+                fontSize: 12,
+              },
+            },
+          },
+          formatter: (name) => {
+            // formatter格式化函数动态呈现数据
+            var total = 0; // 用于计算总数
+            var target; // 遍历拿到数据
+            for (var i = 0; i < this.CjData.length; i++) {
+              total += this.CjData[i].value;
+              if (this.CjData[i].name == name) {
+                target = this.CjData[i].value;
+              }
+            }
+            var v = ((target / total) * 100).toFixed(2);
+            return `{oneone|${name}}  {twotwo|${target}分}   {threethree|${v}%}`;
+            //     富文本第一列样式应用    富文本第二列样式应用      富文本第三列样式应用
+          },
+        },
+        color: ["#baf", "#bfa", "#cde"], // 控制圆环图的颜色
+        series: [
+          {
+            name: "成绩单",
+            type: "pie", // 类型为饼图
+            radius: ["30%", "50%"], // 圆环分为内径和外径，就是两个半径不一样的饼图可组成一个圆环图，radius数组中的两项分别为内径和外径（相对画布的百分比）
+            center: ["32%", "50%"], // 调整圆环图位置
+            data: this.CjData, // 饼图的数据，一般是发请求获取的
+            avoidLabelOverlap: true, // 防止牵引线堆叠挤在一块
+            label: {
+              normal: {
+                show: true,
+                position: "outside", // 另有参数inside，可以让数据在圆环上
+                formatter: "{d}%", //模板变量有 {a}、{b}、{c}、{d}，分别表示系列名，数据名，数据值，百分比。{d}数据会根据value值计算百分比
+                textStyle: {
+                  // 牵引线上的文字的样式
+                  align: "right",
+                  baseline: "middle",
+                  fontFamily: "微软雅黑",
+                  fontSize: 12,
+                  fontWeight: "bolder",
+                  color: "#333",
+                },
+              },
+            },
+            labelLine: {
+              show: true, // 是否显示延展线
+              length: 10, // 延展线条的长度
+            },
+          },
+        ]
+      }
     }
   },
   mounted() {
@@ -232,10 +335,13 @@ export default {
     this.raDar.setOption(this.raDarOption)
     this.bar = this.$echarts.init(this.$refs.bar);
     this.bar.setOption(this.barOption)
+    this.barCj = this.$echarts.init(this.$refs.barCj);
+    this.barCj.setOption(this.barCjOption)
     window.onresize = () => {
       this.funnel.resize();
       this.raDar.resize();
       this.bar.resize();
+      this.barCj.resize()
     }
     setInterval(() => {
       this.nowDate = this.timestampToTime(new Date().getTime())
@@ -285,6 +391,11 @@ export default {
 }
 
 #funnel, #raDar, #bar {
+  width: 100%;
+  height: 50vh;
+  margin-top: 50px;
+}
+#barCj {
   width: 100%;
   height: 50vh;
   margin-top: 50px;
